6f7e19b36b9e13fb3b418948b1b5db9e64467528
besta <Besta111@yandex.ru>
2023-03-17 01:17:29 +0300

    ManyToMany

--- src/main/java/control2/TablesStedentsTeacher.java
+++ src/main/java/control2/TablesStedentsTeacher.java
@@ -4,46 +4,44 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class TablesStedents {
+public class TablesStedentsTeacher {
     public static void main(String[] args) {
         Configuration configuration = new Configuration();
         configuration.configure();
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory()){
-         Session session = sessionFactory.openSession();
-
-         Students students1 = Students.builder()
+            Session session = sessionFactory.openSession();
+            session.beginTransaction();
+            Students students1 = Students.builder()
                  .name("Hris")
                  .surname("Luka")
-                 .id_teacher(1)
                  .build();
-         session.save(students1);
+            session.save(students1);
 
             Students students2 = Students.builder()
                     .name("Fedya")
                     .surname("Volk")
-                    .id_teacher(1)
                     .build();
             session.save(students2);
 
-            Teacher teacher1 = Teacher.builder()
+            Teachers teacher1 = Teachers.builder()
                     .name("Opti")
                     .surname("Konnan")
-                    .id_students(1)
                     .build();
             session.save(teacher1);
 
-            Teacher teacher2 = Teacher.builder()
+            Teachers teacher2 = Teachers.builder()
                     .name("Dima")
                     .surname("Feduk")
-                    .id_students(2)
                     .build();
             session.save(teacher2);
-
+            students1.getTeachers().add(teacher1);
+            students2.getTeachers().add(teacher1);
+            students2.getTeachers().add(teacher2);
+            session.save(students1);
+            session.save(students2);
+            session.getTransaction().commit();
             session.close();
-
-//            Students students = session.get(Students.class,1);
-//            System.out.println(students1);
 
 
 
