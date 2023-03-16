b51aaf7328b2d7f9d234c3107597698d96212d7f
besta <Besta111@yandex.ru>
2023-03-17 01:17:29 +0300

    ManyToMany

--- src/main/java/control2/Teachers.java
+++ src/main/java/control2/Teachers.java
@@ -1,40 +1,32 @@
 package control2;
 
-import jakarta.persistence.*;
-import jakarta.persistence.ManyToMany;
+import javax.persistence.*;
 import lombok.*;
 
-import java.sql.Date;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
+@Data
 @Entity
-@Table(name="teacher")
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class Teacher {
+public class Teachers {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
-    @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "surname", nullable = false)
     private String surname;
 
 //    @Column
 //    private long id_students;
     @Builder.Default
-    @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(name ="studetsTeacher",
-            joinColumns =  @JoinColumn(name = "teacher_id", referencedColumnName = "id"),
-            inverseJoinColumns = @JoinColumn(name = "students_id", referencedColumnName = "id") )
-    private Set<Students> tasks = new HashSet<Students>();
+    @ManyToMany(mappedBy = "teachers")
+    private Set<Students> students = new HashSet<Students>();
 
 }
 
